7fcbc3c540d2e4fbfaa935dd24b61bdfec790b55
Matthias <me@ptweety.net>
2022-10-11 19:29:03 +0200

    remove nextport and hm-discover

---
+++ nodes/lib/nextport.js
@@ -0,0 +1,35 @@
+/**
+ * Based on https://github.com/hobbyquaker/nextport by Sebastian Raff (Hobbyquaker)
+ */
+
+const net = require('node:net');
+
+const getPort = (port, address, cb) => {
+    if (typeof address === 'function') {
+        cb = address;
+        address = '0.0.0.0';
+    }
+
+    const server = net.createServer();
+
+    server.on('listening', () => {
+        server.close();
+    });
+
+    server.on('close', () => {
+        cb(port);
+    });
+
+    server.on('error', () => {
+        port += 1;
+        if (port <= 65_535) {
+            getPort(port, address, cb);
+        } else {
+            cb(null);
+        }
+    });
+
+    server.listen(port, address);
+};
+
+module.exports = getPort;
